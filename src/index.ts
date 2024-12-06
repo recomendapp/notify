89605110762a9564c2521d27810938c230eba8d3
loup <loup.quehec@gmail.com>
2024-12-06 19:04:29 +0100

    adding route

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import 'dotenv/config';
 import App from './app';
 import SubscriberController from './api/v1/subscriber/subscriber.controller';
 import GuidelistController from './api/v1/guidelist/guidelist.controller';
+import FollowerController from './api/v1/follower/follower.controller';
 
 const app = new App([
 	new SubscriberController(),
 	new GuidelistController(),
+	new FollowerController(),
 ]);
 
 app.listen();
